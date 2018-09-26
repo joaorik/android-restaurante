084ab0985b8bad95b6233ee6e6ca40ff4d93a493
Joao <juaorik@gmail.com>
2018-09-26 13:17:51 -0300

    update
    update

--- app/src/main/java/net/leocadio/joao/restaurante/MainActivity.java
+++ app/src/main/java/net/leocadio/joao/restaurante/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Declara os objetos referentes ao elementos no layout
     private EditText mEditTextValorConta, mEditTextDinheiro;
     private TextView mTextViewValorGorjeta, mTextViewTotal, mTextViewTroco;
 
@@ -17,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inicializamos todos os objetos referentes aos elementos do layout
         mEditTextValorConta = findViewById(R.id.editTextValorConta);
         mEditTextDinheiro = findViewById(R.id.editTextDinheiro);
         mTextViewValorGorjeta = findViewById(R.id.textViewValorGorojeta);
@@ -26,20 +24,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calcular(View view) {
-        // Recuperamos os valores digitados
         String valorContaDigitada = mEditTextValorConta.getText().toString();
         String valorDinheiroDigitado = mEditTextDinheiro.getText().toString();
-        // Verificamos se existe algum valor recuperado vazio
-        // Observe o operador lógico negativo !
+
         if (!(valorContaDigitada.isEmpty()) && !(valorDinheiroDigitado.isEmpty())) {
-            // Se os valores não são vazios, converte-os para float
             float valorConta = Float.parseFloat(valorContaDigitada);
             float valorDinheiro = Float.parseFloat(valorDinheiroDigitado);
-            // Realiza os cálculos
+
             float valorGorjeta = valorConta * 15 / 100;
             float total = valorConta + valorGorjeta;
             float troco = valorDinheiro - total;
-            // Exibe os resultados para o usuário
+
             mTextViewValorGorjeta.setText(String.format("R$ %.2f", valorGorjeta));
             mTextViewTotal.setText(String.format("R$ %.2f", total));
             mTextViewTroco.setText(String.format("R$ %.2f", troco));
@@ -47,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void limparDados(View view) {
-        // Limpa todos os EditText no layout
         mEditTextValorConta.setText("");
         mEditTextDinheiro.setText("");
     }
